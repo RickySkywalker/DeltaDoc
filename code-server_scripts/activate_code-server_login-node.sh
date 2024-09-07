87d8fbf7e873b577c2705ef011deeeec2e94f62d
RickySkywalker <80032688+RickySkywalker@users.noreply.github.com>
2024-09-07 13:33:18 -0500

    Update activate_code-server_login-node.sh

--- code-server_scripts/activate_code-server_login-node.sh
+++ code-server_scripts/activate_code-server_login-node.sh
@@ -6,11 +6,11 @@
 ACCOUNT="bckr-dtai-gh"
 PARTITION="ghx4-interactive"
 NODES="1"
-GPUS_PER_NODE="4"
+GPUS_PER_NODE="1"
 TASKS="1"
 TASKS_PER_NODE="1"
 CPUS_PER_TASK="64"
 MEMORY="128g"
-TIME="48:00:00"
+TIME="1:00:00"
 
 nohup srun --account=$ACCOUNT --partition=$PARTITION --nodes=$NODES --gpus-per-node=$GPUS_PER_NODE --tasks=$TASKS --tasks-per-node=$TASKS_PER_NODE --cpus-per-task=$CPUS_PER_TASK --mem=$MEMORY --time=$TIME   --pty bash activate_code-server.sh &
